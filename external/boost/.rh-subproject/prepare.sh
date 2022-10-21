6002861e239e099deeba4367e2e2f71879eab175
ramblehead <v.rybynok@gmail.com>
2022-10-21 10:54:48 +0100

    Checkpoint

--- external/boost/.rh-subproject/prepare.sh
+++ external/boost/.rh-subproject/prepare.sh
@@ -28,3 +28,25 @@ CMD=(echo)
 CMD+=('"using gcc : 11~c++20 : g++-11 : <cxxflags>-std=c++20 ;"')
 CMD+=(">> tools/build/src/user-config.jam")
 echo + "${CMD[@]}" && eval "${CMD[@]}"
+
+CMD=(echo -e)
+CMD+=('"\nimport os ;"')
+CMD+=(">> tools/build/src/user-config.jam")
+echo + "${CMD[@]}" && eval "${CMD[@]}"
+
+CMD=(echo -e)
+CMD+=('"\nlocal ARM_NONE_EABI_TOOLCHAIN = `
+  `[ os.environ ARM_NONE_EABI_TOOLCHAIN ] ;"')
+CMD+=(">> tools/build/src/user-config.jam")
+echo + "${CMD[@]}" && eval "${CMD[@]}"
+
+CMD=(echo -e)
+CMD+=('"\nusing gcc : 11~arm_none_eabi_c++20 :\n`
+  `\$(ARM_NONE_EABI_TOOLCHAIN)/bin/arm-none-eabi-g++ :\n`
+  `<cxxflags>-std=c++20 ;"')
+CMD+=(">> tools/build/src/user-config.jam")
+echo + "${CMD[@]}" && eval "${CMD[@]}"
+
+#using gcc : 11~arm_none_eabi_c++20 :
+#$(ARM_NONE_EABI_TOOLCHAIN)/bin/arm-none-eabi-g++ :
+#<cxxflags>-std=c++20 ;
