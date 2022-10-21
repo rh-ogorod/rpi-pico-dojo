c5cd9e68e08c6fa598676c28ddb57936472d87c7
ramblehead <v.rybynok@gmail.com>
2022-10-21 10:30:00 +0100

    Checkpoint

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+SDPATH="$(dirname "${BASH_SOURCE[0]}")"
+if [[ ! -d "${SDPATH}" ]]; then SDPATH="${PWD}"; fi
+readonly SDPATH="$(cd -P "${SDPATH}" && pwd)"
+
+cd "${SDPATH}/.rh-project"; echo cd "${PWD}"
+
+CMD=(./bootstrap.sh)
+echo + "${CMD[@]}" && "${CMD[@]}"
