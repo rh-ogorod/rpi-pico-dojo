5184770b67f98c60c17ba4fa6e99ea8da8582ef7
ramblehead <v.rybynok@gmail.com>
2022-10-28 14:51:43 +0100

    Checkpoint

--- .rh-project/bootstrap.sh
+++ .rh-project/bootstrap.sh
@@ -7,7 +7,7 @@ SDPATH="$(dirname "${BASH_SOURCE[0]}")"
 if [[ ! -d "${SDPATH}" ]]; then SDPATH="${PWD}"; fi
 readonly SDPATH="$(cd -P "${SDPATH}" && pwd)"
 
-cd "${SDPATH}"; echo cd "${PWD}"
+cd "${SDPATH}"; echo + cd "${PWD}"
 
 # shellcheck source=./conf.sh
 source "${SDPATH}/conf.sh"
@@ -15,34 +15,41 @@ source "${SDPATH}/conf.sh"
 CMD=(./git-populate-submodules.sh)
 echo + "${CMD[@]}" && "${CMD[@]}"
 
-wait
+(echo
+ cd "${PRJ_ROOT_PATH}"; echo + cd "${PWD}"
+
+ echo
+ CMD=(yarn)
+ echo + "${CMD[@]}" && "${CMD[@]}"
+
+ echo
+ CMD=(mkdir -vp tools)
+ echo + "${CMD[@]}" && "${CMD[@]}"
+
+ echo
+ CMD=(mkdir -vp .cache)
+ echo + "${CMD[@]}" && "${CMD[@]}")
+
+(echo
+ cd "${PRJ_ROOT_PATH}/.cache"; echo + cd "${PWD}"
+
+ echo
+ CMD=(wget -c "${TOOLCHAIN_URL}")
+ echo + "${CMD[@]}" && "${CMD[@]}")
+
+(echo
+ cd "${PRJ_ROOT_PATH}/tools"; echo + cd "${PWD}"
+
+ echo
+ CMD=(tar --skip-old-files -xJf "${PRJ_ROOT_PATH}/.cache/${TOOLCHAIN_ARC}")
+ echo + "${CMD[@]}" && "${CMD[@]}"
+
+ echo
+ CMD=(rm "${PRJ_ROOT_PATH}/.cache/${TOOLCHAIN_ARC}")
+ echo + "${CMD[@]}" && "${CMD[@]}")
 
 echo
-cd "${PRJ_ROOT_PATH}"; echo cd "${PWD}"
-
-echo
-CMD=(mkdir -vp tools)
-echo + "${CMD[@]}" && "${CMD[@]}"
-
-echo
-CMD=(mkdir -vp .cache)
-echo + "${CMD[@]}" && "${CMD[@]}"
-
-echo
-cd "${PRJ_ROOT_PATH}/.cache"; echo cd "${PWD}"
-
-echo
-CMD=(wget "${TOOLCHAIN_URL}")
-echo + "${CMD[@]}" && "${CMD[@]}"
-
-cd "${PRJ_ROOT_PATH}/tools"; echo cd "${PWD}"
-
-echo
-CMD=(tar -xJf "${PRJ_ROOT_PATH}/.cache/${TOOLCHAIN_ARC}")
-echo + "${CMD[@]}" && "${CMD[@]}"
-
-echo
-CMD=(rm "${PRJ_ROOT_PATH}/.cache/${TOOLCHAIN_ARC}")
+CMD=(./pre-build.sh)
 echo + "${CMD[@]}" && "${CMD[@]}"
 
 echo
@@ -51,4 +58,8 @@ echo + "${CMD[@]}" && "${CMD[@]}"
 
 echo
 CMD=(./cmake-build.sh)
+echo + "${CMD[@]}" && "${CMD[@]}"
+
+echo
+CMD=(./compdb-unbox.sh)
 echo + "${CMD[@]}" && "${CMD[@]}"
